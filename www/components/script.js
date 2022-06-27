930bb67468243458f34adf3123669ef45b970b5c
GiovannaNO <giovanna10nunes@gmail.com>
2022-06-28 00:07:05 +0900

    Compactando código

--- www/components/script.js
+++ www/components/script.js
@@ -4,6 +4,8 @@ window.onload = function (){
                 mode: 'cors',
                 cache:'default'
         }
+
+        var a ="";
     cotar.addEventListener('click', function(){
         let select = document.querySelector('#select').value;
         fetch(`https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL`, opcoes)
@@ -11,26 +13,20 @@ window.onload = function (){
             response => {response.json()
                 .then(data => {
                     if(select == 'cotar1'){
-                        document.querySelector('#compra').textContent = data.USDBRL.bid;
-                        document.querySelector('#venda').textContent = data.USDBRL.ask;
-                        document.querySelector('#variacao').textContent = data.USDBRL.varBid;
-                        document.querySelector('#maximo').textContent = data.USDBRL.high;
-                        document.querySelector('#minimo').textContent = data.USDBRL.low;
+                        a=data.USDBRL;
                     }
                     else if(select == 'cotar2'){
-                        document.querySelector('#compra').textContent = data.EURBRL.bid;
-                        document.querySelector('#venda').textContent = data.EURBRL.ask;
-                        document.querySelector('#variacao').textContent = data.EURBRL.varBid;
-                        document.querySelector('#maximo').textContent = data.EURBRL.high;
-                        document.querySelector('#minimo').textContent = data.EURBRL.low;
+                        a=data.EURBRL;
                     }
                     else if(select == 'cotar3'){
-                        document.querySelector('#compra').textContent = data.BTCBRL.bid;
-                        document.querySelector('#venda').textContent = data.BTCBRL.ask;
-                        document.querySelector('#variacao').textContent = data.BTCBRL.varBid;
-                        document.querySelector('#maximo').textContent = data.BTCBRL.high;
-                        document.querySelector('#minimo').textContent = data.BTCBRL.low;
+                        a=data.BTCBRL;
                     }
+
+                    document.querySelector('#compra').textContent = a.bid;
+                    document.querySelector('#venda').textContent = a.ask;
+                    document.querySelector('#variacao').textContent = a.varBid;
+                    document.querySelector('#maximo').textContent = a.high;
+                    document.querySelector('#minimo').textContent = a.low;
                 })
             }
         )
